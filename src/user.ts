ad0bdd2c597b00bdebb4d08e309251de5e3aa844
C-Max <ksr-kursk@yandex.ru>
2021-12-27 12:14:35 +0300

    homework1

---
+++ src/user.ts
@@ -0,0 +1,23 @@
+import { renderBlock } from './lib.js';
+
+export function renderUserBlock (userName: string, linkToAvatar: string, favoriteItemsAmount: number) {
+  if (favoriteItemsAmount < 0) favoriteItemsAmount = 0
+
+  const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет';
+  const hasFavoriteItems = favoriteItemsAmount ? true : false;
+
+  renderBlock(
+    'user-block',
+    `
+    <div class="header-container">
+      <img class="avatar" src="${linkToAvatar}" alt="${userName}" />
+      <div class="info">
+          <p class="name">${userName}</p>
+          <p class="fav">
+            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
+          </p>
+      </div>
+    </div>
+    `
+  );
+}
